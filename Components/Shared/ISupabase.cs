631cb806bc084bcc0cd1a404b9b2213b1ea9423e
NoelSaludo <noelsaludo17@gmail.com>
2026-04-14 16:30:09 +0800

    removed ISupabase interface

--- Components/Shared/ISupabase.cs
+++
@@ -1,9 +0,0 @@
-using DotNetEnv;
-
-namespace CBOS.Components.Shared;
-
-public abstract class ISupabase
-{
-    protected readonly Supabase.Client _supabase;
-    public ISupabase(Supabase.Client supabase) { }
-}
